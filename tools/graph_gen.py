e28c051c2bd88ffc7510fb63c88f918f2c18981d
Moenupa WANG <moenupa@outlook.com>
2024-04-23 18:32:07 -0400

    graphing

--- tools/graph_gen.py
+++ tools/graph_gen.py
@@ -1,33 +1,35 @@
 import argparse
 from random import randint, sample
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Generate a random graph')
-    parser.add_argument('-v', '--n-vertex', type=int, default=10, help='No. of Vertices (default: 1000)')
-    parser.add_argument('-e', '--n-edge', type=int, default=10, help='No. of Edges (default: 1000)')
+    parser.add_argument('-v', '--n-vertex', type=int,
+                        default=10, help='No. of Vertices (default: 1000)')
+    parser.add_argument('-e', '--n-edge', type=int, default=10,
+                        help='No. of Edges (default: 1000)')
     return parser.parse_args()
 
 
 def generate(n_vertex: int, n_edge: int):
     graph = [[0 for _ in range(n_vertex)] for _ in range(n_vertex)]
-        
+
     count = 0
     while count < n_edge:
         u, v = sample(range(n_vertex), 2)
         if graph[u][v] == 0:
             graph[u][v] = graph[v][u] = 1
-            count +=1
-            
+            count += 1
+
     for i in range(n_vertex):
         graph[i][i] = -1
 
-    # deg = max(sum(row) for row in graph)
-
-    print(n_vertex, n_edge)
+    deg = max(sum(row) for row in graph)
+    print(n_vertex, n_edge, deg)
 
     for row in graph:
         print(' '.join(map(str, row)))
- 
+
 
 if __name__ == '__main__':
     args = parse_args()
--- tools/graph_plot.py
+++ tools/graph_plot.py
@@ -1,34 +1,92 @@
+import argparse
+import os
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
-def read_matrix(path: str = 'example/1.txt') -> np.ndarray:
+RES_PATH = 'res'
+
+
+def firstfit(mat: np.ndarray) -> np.ndarray:
+    n = mat.shape[0]
+
+    colormap = np.zeros(n, dtype=int)
+    color_used = [False] * (n+1)
+
+    for i in range(n):
+        for j in range(n):
+            if mat[i][j] == 1 and colormap[j] != 0:
+                color_used[colormap[j]] = True
+
+        for j in range(1, n+1):
+            if not color_used[j]:
+                colormap[i] = j
+                break
+
+        for j in range(n):
+            if mat[i][j] == 1 and colormap[j] != 0:
+                color_used[colormap[j]] = False
+
+    return colormap
+
+
+def read_matrix(path: str) -> np.ndarray:
     mat = np.genfromtxt(path, delimiter=' ', skip_header=1)
-    
-    # if the graph is not colored
-    if mat[0][0] == -1:
-        for i in range(mat.shape[0]):
-            mat[i][i] = i
-            
     return mat
 
 
-def visualize(mat: np.ndarray):
+def visualize(mat: np.ndarray, do_coloring: bool = False) -> None:
+    os.makedirs(RES_PATH, exist_ok=True)
     n = mat.shape[0]
-    
+
     edges = [(i, j) for i in range(n) for j in range(i) if mat[i][j] == 1]
     G = nx.Graph()
     G.add_nodes_from([i for i in range(n)])
     G.add_edges_from(edges)
-    
-    if n < 300:
-        nx.draw(G, node_color=[mat[i][i] for i in range(n)], 
-                with_labels=True, font_color='whitesmoke')
-        plt.savefig('res/test.png')
-        
-        
+
+    if do_coloring:
+        colormap = firstfit(mat)
+    else:
+        colormap = np.arange(n)
+    colormark = 'c' if do_coloring else 'x'
+
+    if n <= 300:
+        plt.figure(figsize=(8, 6))
+        plt.title(f'V={n} C={colormap.max()} Graph')
+        nx.draw(G, node_color=colormap.tolist(),
+                with_labels=(n < 50), font_color='whitesmoke',
+                node_size=(300 if n < 50 else 100),
+                edge_color='#999')
+        plt.savefig(
+            f'{RES_PATH}/{n}_{colormark}_graph.png',
+            dpi=300
+        )
+        plt.clf()
+
+    if n <= 1000:
+        plt.figure(figsize=(8, 6))
+        plt.title(f'V={n} C={colormap.max()} Heatmap')
+        sns.heatmap(mat, xticklabels=False, yticklabels=False,
+                    cmap=sns.light_palette("seagreen", as_cmap=True))
+        plt.savefig(
+            f'{RES_PATH}/{n}_{colormark}_heatmap.png',
+            dpi=300
+        )
+        plt.clf()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Visualize a graph')
+    parser.add_argument('-f', '--file', type=str, metavar='PATH', required=True,
+                        help='Paht of matrix file')
+    parser.add_argument('-c', '--color', dest='coloring', action='store_true',
+                        help='Generate coloring')
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    file = read_matrix()
-    visualize(file)
-    print(file)
+    args = parse_args()
+    mat = read_matrix(args.file)
+    visualize(mat, args.coloring)
